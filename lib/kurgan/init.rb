220fe36ba8078c8dace741ceffe0f4bcafd92070
Guslington <guslington@gmail.com>
2021-08-11 21:40:09 +1000

    cleanup init

--- lib/kurgan/init.rb
+++ lib/kurgan/init.rb
@@ -36,7 +36,7 @@ module Kurgan
 
     def create_cfhighlander_template
       opts = {parameters: @parameters, components: @components, description: @description}
-      template('templates/cfhighlander.rb.tt', "#{@dir}/#{name}.cfhighlander.rb", opts)
+      template 'templates/cfhighlander.rb.tt', "#{@dir}/#{name}.cfhighlander.rb", opts
     end
 
     def create_config_yaml
@@ -48,7 +48,7 @@ module Kurgan
     end
 
     def git_init
-      if yes?("git init project?")
+      if yes? "git init project?"
         run "git init #{@dir}"
         template 'templates/gitignore.tt', "#{@dir}/.gitignore"
       else
@@ -57,16 +57,18 @@ module Kurgan
     end
 
     def ci_init
-      if yes?("Setup a CI pipeline?")
+      if yes? "Setup a CI pipeline?"
         ci = ask "CI flavour", limited_to: ['github', 'jenkins', 'codebuild']
         case ci
         when 'github'
-          template('templates/github_actions.yaml.tt', "#{@dir}/.github/workflows/rspec.yaml")
-          insert_into_file "README.md", "![cftest](https://github.com/theonestack/hl-component-#{name}/actions/workflows/rspec.yaml/badge.svg)", :after => /#/
+          template 'templates/github_actions.yaml.tt', "#{@dir}/.github/workflows/rspec.yaml"
+          insert_into_file "#{@dir}/README.md", 
+            "![cftest](https://github.com/theonestack/hl-component-#{name}/actions/workflows/rspec.yaml/badge.svg)\n\n", 
+            :after => /^# #{name} CfHighlander Component\n\n/
         when 'jenkins'
-          template('templates/Jenkinsfile.tt', "#{@dir}/Jenkinsfile")
+          template 'templates/Jenkinsfile.tt', "#{@dir}/Jenkinsfile"
         when 'codebuild'
-          template('templates/codebuild.yaml.tt', "#{@dir}/.codebuild.yaml")
+          template 'templates/codebuild.yaml.tt', "#{@dir}/.codebuild.yaml"
         end
       else
         say "Skipping CI setup", :yellow
